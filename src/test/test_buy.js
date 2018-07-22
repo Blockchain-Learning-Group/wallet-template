0de7a6b2426e892b77cd5a035915c9e6edc70300
Adam J Lemmon <adamjlemmon@gmail.com>
2018-07-22 16:51:06 -0400

    v2 template updated

---
+++ src/test/test_buy.js
@@ -0,0 +1,44 @@
+// Import the token build artifacts
+
+
+contract('Token.buy()', accounts => {
+  // Define the owner account
+
+
+  it("should buy new tokens.", async () => {
+    // Create a new instance of the token, deployed from the owner
+
+    // Specify the wei value
+
+
+    // Send the transaction to the token's buy method
+
+
+    // Pull the rate from the contract to compute how may tokens should be bought and minted
+
+
+    // Compute the token amount, wei * (token / wei)
+
+
+    // Event emission
+
+
+    // Assert correct values were emitted
+
+
+
+
+
+    // Balance
+
+
+
+    // Total Supply
+
+
+  })
+
+  /**
+   * Add further test cases below
+   */
+})
